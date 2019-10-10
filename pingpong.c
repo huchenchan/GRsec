6643ab2cb79e661d4614aff44345525d58a9f72c
HuanC <56388347+huchenchan@users.noreply.github.com>
2019-10-10 11:37:43 +0200

    Create pingpong.c

---
+++ pingpong.c
@@ -0,0 +1,9 @@
+using System.Net.NetworkInformation;
+
+Ping sender = new Ping();
+PingReply reply = sender.Send ("www.example.com");
+
+if (reply.Status == IPStatus.Success)
+{
+  Console.WriteLine("Ping successful.");
+}
